941f2bf5ffcb1a524abe21ac5f1d267c1fe0319b
gerogarcia03 <gerogarcia2003@gmail.com>
2022-12-02 19:55:22 -0300

    classes

--- productManager.js
+++ productManager.js
@@ -29,15 +29,17 @@ class ProductManager {
     }
 
     getProductById(id) {
-        const prod = this.products.find((prod) => prod.id === id)
-        if (id != id) {
-            console.log("Not Found")
-        };
-        this.products.push(prod)
+        const prod = this.products.find((prod) => prod.id === id);
+        if (prod === id) {
+            console.log("Not Found");
+        }
+        else{
+            this.products.push(prod)
+        }
     }
 }
 
 const productManager = new ProductManager();
 productManager.addProduct("macbook pro", "notebook gama alta", 200, "no disponible", "123dasd", 2);
 productManager.addProduct("amd ryzen 5 5600x", "procesador gama media", 233, "no disponible", "123dasd", 2);
-console.log(productManager.getProduct());
+console.log(productManager.getProductById());
