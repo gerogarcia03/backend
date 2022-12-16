09da84d9425c9021b28ac750a360fb4697c1046c
gerogarcia03 <gerogarcia2003@gmail.com>
2022-12-15 23:22:17 -0300

    manejo de archivos

--- productManager.js
+++ productManager.js
@@ -1,45 +1,22 @@
+const { rejects } = require('assert');
+const fs = require('fs');
+const path = require('path')
+
 class ProductManager {
     constructor() {
         this.products = [];
+        this.path = path.join(__dirname, "products.json") 
     }
 
-    addProduct(
-        title,
-        description,
-        price,
-        thumbnail,
-        code,
-        stock,
-    ) {
-        const product = {
-            id: this.products.length + 1,
-            title,
-            description,
-            price,
-            thumbnail,
-            code,
-            stock,
-        };
-        if (
-            title === undefined ||
-            description === undefined ||
-            price === undefined ||
-            thumbnail === undefined ||
-            code === undefined ||
-            stock === undefined
-        ) { console.log("all fields are required"); }
-
-
-
-        let validation = this.products.find((product) => product.code === code);
-        (validation) ? console.log("Product already exists") : this.products.push(product)
+    addProduct( prod ) {
+        return new Promise((resolve, rejects))
     }
 
     getProducts() {
         return this.products;
     }
 
-    getProductById(id) {
+    getProductsById(id) {
         const prod = this.products.find((product) => product.id === id);
         if (prod) {
             return prod;
@@ -53,4 +30,4 @@ const productManager = new ProductManager();
 productManager.addProduct("macbook pro", "notebook gama alta", 200, "no disponible", "123abc", 2);
 productManager.addProduct("amd ryzen 5 5600x", "procesador gama media", 230, "no disponible", "123abcd", 2);
 productManager.addProduct("intel i5 6500", "procesador gama media", 230, "no disponible", "abc1235", 10)
-console.log("product search", productManager.getProducts());
+console.log("product search", productManager.getProductsById());
