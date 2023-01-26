4721fa6c0d8de9ca7f666d9c34fe11643425e336
gerogarcia03 <gerogarcia2003@gmail.com>
2023-01-26 12:04:41 -0300

    motores de plantilla

--- src/app.js
+++ src/app.js
@@ -1,18 +1,58 @@
+import { engine } from "express-handlebars";
 import express from "express";
-import prodRouter from "./routes/prod.router.js";
-import cartRouter from "./routes/cart.router.js";
+import viewsRoute from "./routes/view.router.js";
+import { Server } from "socket.io";
 
 const app = express();
+const port = process.env.PORT || 8080;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-app.use("/products", prodRouter);
-app.use("/cart", cartRouter)
+const messages = [];
 
-
-const port = process.env.PORT || 8080
-
-app.listen(port, () => {
-    console.log(`Iniciando en http://localhost:${port}`)
+const httpServer = app.listen(port, () => {
+  console.log(`El servidor esta corriendo en http//:localhost${port}`);
 });
+
+const socketServer = new Server(httpServer);
+
+socketServer.on("connection", (socket) => {
+  console.log("Nuevo cliente conectado!");
+  socket.emit("message", "Bienvenido al servidor!");
+  socket.on("message", (data) => {
+    console.log(data);
+  });
+
+  socket.on("new-message", (data) => {
+    console.log(data);
+    messages.push(data);
+
+    socketServer.emit("messages", messages);
+  });
+});
+
+// get messages from index.js
+
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./src/views");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    let homePage = {
+        title: "HomePage"
+    }
+    res.render("home", homePage)
+});
+
+
+app.get("/form", (req, res) => {
+  let testUser = {
+    title: "Este es un formulario",
+    message: "Hola gracias por registrarte!",
+    name: "Jose",
+  };
+  res.render("form", testUser);
+});
+
+app.get("/prods", (req, res) => {
+    res.render("prods")
+})
